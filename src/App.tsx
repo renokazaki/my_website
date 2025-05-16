5963383b18f610bc4cc4aaade00b04e5d46013a7
ren <ren84690@gmail.com>
2025-05-17 00:26:10 +0900

    各仮セクションの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
  
 import { Canvas } from '@react-three/fiber';
 import Experience from './_components/Experience';
+import { ScrollControls } from '@react-three/drei';
+import { config } from './config';
  
 function App() {
   return (
     <Canvas camera={{ position: [0, 0.5, 5], fov: 42 }}>
     <color attach="background" args={["#f5f3ee"]} />
     <fog attach="fog" args={["#f5f3ee", 10, 50]} />
+    <ScrollControls
+          pages={config.sections.length}
+          damping={0.1}
+          maxSpeed={0.2}
+        >
     <group position-y={-1}>
       <Experience />
     </group>
+    </ScrollControls>
   </Canvas>
   );
 }
--- src/_components/Experience.tsx
+++ src/_components/Experience.tsx
@@ -1,14 +1,47 @@
-import { Environment } from "@react-three/drei";
+import { Environment ,useScroll } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import Star from "./Star";
+import { useRef } from "react";
+import * as THREE from "three";
+import SectionTitle from "./SectionTitle/SectionTitle";
+
+const SECTIONS_DISTANCE = 10;
+
 
 export default function Experience() {
-  return (
+  const sceneContainer = useRef<THREE.Group | null>(null);
+  const scrollData = useScroll();
+  useFrame(() => {
+    if (sceneContainer.current) {
+      sceneContainer.current.position.z =
+        -scrollData.offset * SECTIONS_DISTANCE * (scrollData.pages - 1);
+    }
+  });
+    return (
     <>
       <Environment preset="sunset" />
       <mesh>
         <boxGeometry />
         <meshStandardMaterial color="white" />
         <Star position={[1, 1, 1]} />
+        <group ref={sceneContainer}>
+        {/* HOME */}
+        <group>
+          <SectionTitle position-x={0.5}>HOME</SectionTitle>
+        </group>
+        {/* SKILLS */}
+        <group position-z={SECTIONS_DISTANCE}>
+          <SectionTitle position-x={0.5}>SKILLS</SectionTitle>
+        </group>
+        {/* PROJECTS */}
+        <group position-z={2 * SECTIONS_DISTANCE}>
+          <SectionTitle position-x={0.5}>PROJECTS</SectionTitle>
+        </group>
+        {/* CONTACT */}
+        <group position-z={3 * SECTIONS_DISTANCE}>
+          <SectionTitle position-x={0.5}>CONTACT</SectionTitle>
+        </group>
+      </group>
       </mesh>
     </>
   );
